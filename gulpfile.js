d900dadd306df1c88ef66d09b1781cf19a28cf5d
noby <n.fujioka@abacusmaths.info>
2016-12-03 21:01:17 +0000

    adds auto prefixer for brower compatibility

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
 var browserSync = require('browser-sync').create();
 
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
 gulp.task('sass', function(){
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass())
+  return gulp
+    .src('app/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({
       stream: true
