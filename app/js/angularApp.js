10211d7e601a8e4c1e42a9d8c1f15808d3bdd8a7
noby <n.fujioka@abacusmaths.info>
2016-12-05 00:45:29 +0000

    working on angular ui router.

--- app/js/angularApp.js
+++ app/js/angularApp.js
@@ -1,6 +1,22 @@
 var app = angular.module('myApp',['ngAnimate', 'ui.router']);
 
+app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url:'/',
+      template: '<h1>Home</h1>'
+    })
+    .state('about', {
+      url:'/about',
+      template: '<h1>About</h1>'
+    });
+
+});
+
 app.controller('mainCtrl', function(){
   var self = this;
   self.message = "Hello, World!";
+  self.list = [{name:'Dog', number: 4}, {name:'cat', number: 8}, {name:'mouse', number: 6}];
 });
