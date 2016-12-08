55b4a2d6b4c0a316521107a2ea3b72885d9504ca
noby <n.fujioka@abacusmaths.info>
2016-12-08 02:46:44 +0000

    adds more styles and pages

--- app/js/angularApp.js
+++ app/js/angularApp.js
@@ -4,24 +4,62 @@ app.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
-    .state('home', {
+    .state('landing', {
       url:'/',
+      templateUrl: 'static/landing.html',
+      controller: 'landingCtrl as landCtrl'
+    })
+    .state('home', {
+      url:'/home',
       templateUrl: 'static/home.html',
-      controller: 'mainCtrl as mCtrl'
+      controller: 'homeCtrl'
     })
     .state('about', {
       url:'/about',
-      templateUrl: 'static/about.html'
+      templateUrl: 'static/about.html',
+      controller: 'aboutCtrl'
+    })
+    .state('image-gallery', {
+      url:'/image-gallery',
+      templateUrl: 'static/image-gallery.html',
+      controller: 'imageGalleryCtrl as igCtrl'
     })
     .state('contact', {
       url:'/contact',
-      templateUrl: 'static/contact.html'
+      templateUrl: 'static/contact.html',
+      controller: 'contactCtrl'
     });
 
 });
 
-app.controller('mainCtrl', function(){
+app.controller('mainCtrl', function($scope){
+  $scope.parent = {};
+  $scope.parent.addClass = "";
+});
+
+app.controller('landingCtrl', function($scope){
   var self = this;
+  $scope.parent.addClass = "landingBackground";
+});
+
+app.controller('homeCtrl', function($scope){
+  var self = this;
+  $scope.parent.addClass = "";
   self.message = "Hello, World!";
   self.list = [{name:'Dog', number: 4}, {name:'cat', number: 8}, {name:'mouse', number: 6}];
+});
+
+app.controller('aboutCtrl', function($scope){
+  var self = this;
+  $scope.parent.addClass = "";
+});
+
+app.controller('imageGalleryCtrl', function($scope){
+  var self = this;
+  $scope.parent.addClass = "";
+});
+
+app.controller('contactCtrl', function($scope){
+  var self = this;
+  $scope.parent.addClass = "";
 });
